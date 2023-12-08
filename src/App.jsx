40bee0461a2c4f09944fccbb0f94d2e03e3330ed
vel-3633f <vel3633fu@gmail.com>
2023-12-08 13:00:33 +0900

    pokemon data 取得

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(initialURL);
+      loadPokemon(res.results);
+      setLoading(false);
+    };
+    fetchPokemonData();
+  }, []);
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex flex-col w-screen min-h-screen justify-center items-center">
+      {loading ? (
+        <h1>ローディング...</h1>
+      ) : (
+        pokemonData.map((pokemon, index) => {
+          return <Card key={index} pokemon={pokemon}></Card>;
+        })
+      )}
+    </div>
   );
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+const Card = ({ pokemon }) => {
+  console.log(pokemon)
+  return (
+    <div>{pokemon.name}</div>
+  )
+};
+
+export default Card;
